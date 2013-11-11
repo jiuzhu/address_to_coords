831b3077e670e77326284d1b7000cc1aa023d845
Julia Wang <jiuzhu@gmail.com>
2013-11-10 19:59:01 -0500

    Address to Coordinates
    This code gets us coordinates from address filled in on form.

--- app/controllers/addresses_controller.rb
+++ app/controllers/addresses_controller.rb
@@ -3,14 +3,14 @@ require 'json'
 
 class AddressesController < ApplicationController
   def fetch_coordinates
-    @address = "the corner of Foster and Sheridan"
-    @url_safe_address = URI.encode(@address)
-
-    # Your code goes here.
-    # url = ?
-    # raw_data = ?
-    # parsed_data = ?
-    # @latitude = ?
-    # @longitude = ?
+    @address = params[:the_address]
+    if @address != nil
+        @url_safe_address = URI.encode(@address)
+        url = "http://maps.googleapis.com/maps/api/geocode/json?address=" + @url_safe_address + "&sensor=false"
+        raw_data = open(url).read
+        parsed_data = JSON.parse(raw_data)
+        @latitude = parsed_data["results"][0]["geometry"]["location"]["lat"]
+        @longitude = parsed_data["results"][0]["geometry"]["location"]["lng"]
+    end
   end
 end
